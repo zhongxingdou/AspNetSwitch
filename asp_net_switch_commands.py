3fd3c95298754c5d2be5d9e65534eb285ad3f443
zhongxingdou <zhongxingdou@gmail.com>
2013-09-22 10:36:04 +0800

    updated: the extention name is case-sensitive when switching file

--- asp_net_switch_commands.py
+++ asp_net_switch_commands.py
@@ -3,14 +3,15 @@ import os.path
 
 class SwitchAspxCsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        name, ext = os.path.splitext(self.view.file_name())
+        name, real_ext = os.path.splitext(self.view.file_name())
         win = self.view.window()
+        ext = real_ext.lower()
         if ext == ".aspx":
-            win.open_file(name+".aspx.cs")
+            win.open_file(name+ real_ext + ".cs")
         elif ext == ".ascx":
-            win.open_file(name+".ascx.cs")            
+            win.open_file(name+ real_ext + ".cs")            
         elif ext == ".master":
-            win.open_file(name+".master.cs")
+            win.open_file(name+ real_ext + ".cs")
         elif ext == ".cs":
             name2, ext2 = os.path.splitext(name)
             if ext2 == ".designer":
